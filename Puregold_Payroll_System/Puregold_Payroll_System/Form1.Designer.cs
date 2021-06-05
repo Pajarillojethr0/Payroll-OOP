82490f96ef19782363a11c374506b0fc752ff45e
[Pajarillo_Jethro] <79455524+Pajarillojethr0@users.noreply.github.com>
2021-06-05 19:34:44 +0800

    Add files via upload

---
+++ Puregold_Payroll_System/Puregold_Payroll_System/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace Puregold_Payroll_System
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginForm));
+            this.tb_User = new System.Windows.Forms.TextBox();
+            this.tb_Pass = new System.Windows.Forms.TextBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lbl_Display = new System.Windows.Forms.Label();
+            this.picBox_Hide = new System.Windows.Forms.PictureBox();
+            this.picBox_Show = new System.Windows.Forms.PictureBox();
+            this.tb_PlaceHolder = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Hide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Show)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_User
+            // 
+            this.tb_User.BackColor = System.Drawing.SystemColors.Window;
+            this.tb_User.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_User.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_User.ForeColor = System.Drawing.Color.DimGray;
+            this.tb_User.Location = new System.Drawing.Point(148, 204);
+            this.tb_User.Name = "tb_User";
+            this.tb_User.Size = new System.Drawing.Size(258, 22);
+            this.tb_User.TabIndex = 0;
+            this.tb_User.TabStop = false;
+            this.tb_User.Text = "Enter Username";
+            this.tb_User.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb_User.TextChanged += new System.EventHandler(this.tb_User_TextChanged);
+            this.tb_User.Enter += new System.EventHandler(this.tb_User_Enter);
+            this.tb_User.Leave += new System.EventHandler(this.tb_User_Leave);
+            // 
+            // tb_Pass
+            // 
+            this.tb_Pass.BackColor = System.Drawing.SystemColors.Window;
+            this.tb_Pass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_Pass.ForeColor = System.Drawing.Color.Black;
+            this.tb_Pass.Location = new System.Drawing.Point(148, 245);
+            this.tb_Pass.Name = "tb_Pass";
+            this.tb_Pass.Size = new System.Drawing.Size(258, 19);
+            this.tb_Pass.TabIndex = 0;
+            this.tb_Pass.TabStop = false;
+            this.tb_Pass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb_Pass.UseSystemPasswordChar = true;
+            this.tb_Pass.TextChanged += new System.EventHandler(this.tb_Pass_TextChanged_1);
+            this.tb_Pass.Enter += new System.EventHandler(this.tb_Pass_Enter);
+            this.tb_Pass.Leave += new System.EventHandler(this.tb_Pass_Leave);
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btn_Login.FlatAppearance.BorderSize = 0;
+            this.btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Login.Font = new System.Drawing.Font("Arial Unicode MS", 12F);
+            this.btn_Login.Location = new System.Drawing.Point(148, 324);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(258, 33);
+            this.btn_Login.TabIndex = 1;
+            this.btn_Login.Text = "Log in";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            this.btn_Login.MouseEnter += new System.EventHandler(this.btn_Login_MouseEnter);
+            this.btn_Login.MouseLeave += new System.EventHandler(this.btn_Login_MouseLeave);
+            this.btn_Login.MouseHover += new System.EventHandler(this.btn_Login_MouseHover);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Bright", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(146, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(260, 36);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Payroll System";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(79, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(403, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel1.Location = new System.Drawing.Point(148, 224);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 3);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel2.Location = new System.Drawing.Point(148, 265);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(258, 3);
+            this.panel2.TabIndex = 9;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.BackColor = System.Drawing.Color.White;
+            this.btn_Exit.FlatAppearance.BorderSize = 0;
+            this.btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Exit.Font = new System.Drawing.Font("Arial Unicode MS", 12F);
+            this.btn_Exit.Location = new System.Drawing.Point(148, 363);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(258, 33);
+            this.btn_Exit.TabIndex = 2;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = false;
+            this.btn_Exit.Click += new System.EventHandler(this.button2_Click);
+            this.btn_Exit.MouseEnter += new System.EventHandler(this.btn_Exit_MouseEnter);
+            this.btn_Exit.MouseLeave += new System.EventHandler(this.btn_Exit_MouseLeave);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(148, 193);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(148, 235);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lbl_Display
+            // 
+            this.lbl_Display.AutoSize = true;
+            this.lbl_Display.Font = new System.Drawing.Font("Arial Unicode MS", 8.5F);
+            this.lbl_Display.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Display.Location = new System.Drawing.Point(149, 280);
+            this.lbl_Display.Name = "lbl_Display";
+            this.lbl_Display.Size = new System.Drawing.Size(42, 16);
+            this.lbl_Display.TabIndex = 13;
+            this.lbl_Display.Text = "label1";
+            this.lbl_Display.Visible = false;
+            this.lbl_Display.Click += new System.EventHandler(this.lbl_Display_Click);
+            // 
+            // picBox_Hide
+            // 
+            this.picBox_Hide.Image = ((System.Drawing.Image)(resources.GetObject("picBox_Hide.Image")));
+            this.picBox_Hide.Location = new System.Drawing.Point(381, 240);
+            this.picBox_Hide.Name = "picBox_Hide";
+            this.picBox_Hide.Size = new System.Drawing.Size(25, 25);
+            this.picBox_Hide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox_Hide.TabIndex = 14;
+            this.picBox_Hide.TabStop = false;
+            this.picBox_Hide.Click += new System.EventHandler(this.picBox_Hide_Click);
+            // 
+            // picBox_Show
+            // 
+            this.picBox_Show.Image = ((System.Drawing.Image)(resources.GetObject("picBox_Show.Image")));
+            this.picBox_Show.Location = new System.Drawing.Point(381, 240);
+            this.picBox_Show.Name = "picBox_Show";
+            this.picBox_Show.Size = new System.Drawing.Size(25, 25);
+            this.picBox_Show.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox_Show.TabIndex = 15;
+            this.picBox_Show.TabStop = false;
+            this.picBox_Show.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // tb_PlaceHolder
+            // 
+            this.tb_PlaceHolder.BackColor = System.Drawing.SystemColors.Window;
+            this.tb_PlaceHolder.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_PlaceHolder.Font = new System.Drawing.Font("Arial Unicode MS", 12F);
+            this.tb_PlaceHolder.ForeColor = System.Drawing.Color.DimGray;
+            this.tb_PlaceHolder.Location = new System.Drawing.Point(185, 241);
+            this.tb_PlaceHolder.Name = "tb_PlaceHolder";
+            this.tb_PlaceHolder.Size = new System.Drawing.Size(185, 22);
+            this.tb_PlaceHolder.TabIndex = 16;
+            this.tb_PlaceHolder.TabStop = false;
+            this.tb_PlaceHolder.Text = "Enter Password";
+            this.tb_PlaceHolder.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb_PlaceHolder.TextChanged += new System.EventHandler(this.tb_PlaceHolder_TextChanged);
+            this.tb_PlaceHolder.Enter += new System.EventHandler(this.tb_PlaceHolder_Enter);
+            this.tb_PlaceHolder.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_PlaceHolder_KeyPress);
+            this.tb_PlaceHolder.Leave += new System.EventHandler(this.tb_PlaceHolder_Leave);
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(566, 413);
+            this.ControlBox = false;
+            this.Controls.Add(this.tb_PlaceHolder);
+            this.Controls.Add(this.picBox_Show);
+            this.Controls.Add(this.picBox_Hide);
+            this.Controls.Add(this.lbl_Display);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.tb_Pass);
+            this.Controls.Add(this.tb_User);
+            this.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "loginForm";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.loginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Hide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Show)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tb_User;
+        private System.Windows.Forms.TextBox tb_Pass;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lbl_Display;
+        private System.Windows.Forms.PictureBox picBox_Hide;
+        private System.Windows.Forms.PictureBox picBox_Show;
+        private System.Windows.Forms.TextBox tb_PlaceHolder;
+    }
+}
+
---
+++ Puregold_Payroll_System/Puregold_Payroll_System/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Puregold_Payroll_System
+{
+    public partial class loginForm : Form
+    {
+        private OleDbConnection con;
+        private OleDbCommand cmd = new OleDbCommand();
+        OleDbDataAdapter da;
+        DataTable dt = new DataTable();
+
+        private string conParam = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source =C:\Users\jeivee\Desktop\2nd Year\2nd Sem\Object Oriented Programming\Puregold_Payroll_System\Payroll.accdb";
+
+        public loginForm()
+        {
+            con = new OleDbConnection(conParam);
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cb_ShowPass_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var msgBox = MessageBox.Show("Are you sure you want to Exit?", "Warning!!", MessageBoxButtons.YesNo);
+
+            if (msgBox == DialogResult.Yes)
+            {
+                this.Close();
+            }
+
+
+        }
+
+        private void tb_User_Enter(object sender, EventArgs e)
+        {
+            if (tb_User.Text == "Enter Username")
+            {
+                tb_User.Text = "";
+                tb_User.ForeColor = Color.Black;
+               
+            }
+            if(tb_Pass.Text == "")
+            {
+                tb_PlaceHolder.Visible = true;
+            }
+
+        }
+
+        private void tb_User_Leave(object sender, EventArgs e)
+        {
+            if (tb_User.Text == "")
+            {
+                tb_User.Text = "Enter Username";
+                tb_User.ForeColor = Color.DimGray;
+            }
+
+        }
+
+        private void tb_Pass_Enter(object sender, EventArgs e)
+        {
+            if (tb_Pass.Text == "Enter Password")
+            {
+                
+                tb_Pass.Text = "";
+                tb_Pass.ForeColor = Color.Black;
+                tb_Pass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void tb_Pass_Leave(object sender, EventArgs e)
+        {
+            if (tb_Pass.Text == "" && tb_Pass.Focus() == false)
+            {
+                tb_Pass.ForeColor = Color.DimGray;
+                tb_Pass.UseSystemPasswordChar = false;
+                tb_Pass.Visible = false;
+                tb_PlaceHolder.Visible = true;
+            }
+        }
+
+        private void loginForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tb_Pass_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tb_User_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+
+         
+
+                da = new OleDbDataAdapter("SELECT * FROM Login WHERE Username='" + tb_User.Text + "' and Password='" + tb_Pass.Text + "'", con);
+                da.Fill(dt);
+                if (dt.Rows.Count <= 0)
+                {
+                      if (tb_User.Text == "" && tb_Pass.Text == "")
+                      {
+                            lbl_Display.Visible = true;
+                            lbl_Display.Text = "Enter your Username and Password!";
+
+                      }
+                      else
+                      {
+                            lbl_Display.Visible = true;
+                            lbl_Display.Text = "Incorrect Username or Password";
+                      }
+                }
+                else if (dt.Rows.Count > 0)
+                {
+
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                    lbl_Display.Text = "Login Successfully";
+
+
+                    lbl_Display.Text = "";
+
+                }
+
+                dt.Clear();
+            
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            picBox_Hide.Visible = true;
+            picBox_Show.Visible =false;
+            tb_Pass.UseSystemPasswordChar = false;
+
+        }
+
+        private void picBox_Hide_Click(object sender, EventArgs e)
+        {
+            picBox_Show.Visible = true;
+            picBox_Hide.Visible = false;
+            tb_Pass.UseSystemPasswordChar = true;
+        }
+
+        private void lbl_Display_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_PlaceHolder_Enter(object sender, EventArgs e)
+        {
+           tb_Pass.Visible = true;
+            
+            tb_Pass.Focus();
+            tb_PlaceHolder.Visible = false;
+        }
+
+        private void tb_Pass_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_PlaceHolder_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_PlaceHolder_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void btn_Login_MouseHover(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btn_Login_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Login.BackColor = Color.RoyalBlue;
+        }
+
+        private void btn_Login_MouseEnter(object sender, EventArgs e)
+        {
+            btn_Login.BackColor = Color.DeepSkyBlue;
+            if (tb_Pass.Text == "")
+            {
+                tb_PlaceHolder.Visible = true;
+                btn_Login.Focus();
+            }
+        }
+
+        private void btn_Exit_MouseEnter(object sender, EventArgs e)
+        {
+            btn_Exit.BackColor = Color.Red;
+            if (tb_Pass.Text == "")
+            {
+                tb_PlaceHolder.Visible = true;
+                btn_Exit.Focus();
+            }
+        }
+
+        private void btn_Exit_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Exit.BackColor = Color.White;
+        }
+
+        private void tb_PlaceHolder_Leave(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
